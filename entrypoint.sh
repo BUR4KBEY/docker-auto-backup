317a9cc455cf668a1765395dde728507f2a81d37
BUR4KBEY <rectfc@gmail.com>
2024-01-26 18:39:38 +0300

    feat: trust gpg keys and load env variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-# Start cron daemon
-service cron start
+printenv > /etc/environment
+
+# Import GPG public keys from /gpg folder
+for pubkey_file in /gpg/*.gpg; do
+  gpg --import "$pubkey_file"
+done
+
+# Ensure that the cron.log file exists
+touch /app/cron.log
 
 # Schedule the job
-echo "0 4 * * * /app/rust-backup-generator > /app/cron.log 2>&1" > /etc/crontab
+echo "*/5 * * * * cd /app && ./rust-backup-generator > /app/cron.log 2>&1" > /etc/crontab
+crontab /etc/crontab
+
+# Start cron daemon
+service cron start
 
 if [ "$RUN_AT_STARTUP" = "true" ]; then
   /app/rust-backup-generator >> /app/cron.log 2>&1
