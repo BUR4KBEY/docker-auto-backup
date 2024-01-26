e5a91db112ffe54455e58fda6216b2d3dcd63890
BUR4KEY <rectfc@gmail.com>
2024-01-26 21:47:48 +0300

    feat: add ZSTD_COMPRESSION_LEVEL env variable

--- generate_backup.sh
+++ generate_backup.sh
@@ -2,6 +2,10 @@
 
 : ${BACKUP_FOLDER_PATH:=./backup}
 
+if ! [[ "$ZSTD_COMPRESSION_LEVEL" =~ ^[0-9]+$ ]] || ((ZSTD_COMPRESSION_LEVEL < 1 || ZSTD_COMPRESSION_LEVEL > 22)); then
+    ZSTD_COMPRESSION_LEVEL=19
+fi
+
 check_var() {
   local var_name="$1"
 
@@ -19,7 +23,7 @@ cp -r $BACKUP_FOLDER_PATH temp
 
 tar -cf temp/backup.tar -C temp backup
 
-zstd -19 temp/backup.tar
+zstd "-$ZSTD_COMPRESSION_LEVEL" temp/backup.tar
 
 gpg --always-trust --encrypt --recipient $GPG_RECIPIENT --out temp/backup.tar.zst.gpg temp/backup.tar.zst
 
