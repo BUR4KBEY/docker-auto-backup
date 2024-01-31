42a683281fe354090c87a110378608ee3198b7f6
BUR4KBEY <rectfc@gmail.com>
2024-01-31 16:26:14 +0300

    fix: append new logs instead of overwriting

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ printenv > /etc/environment
 
 : ${CRON_SYNTAX:='0 4 * * *'}
 
-RUN_SCRIPT="cd /app && ./docker-auto-backup > /app/cron.log 2>&1"
+RUN_SCRIPT="cd /app && ./docker-auto-backup >> /app/cron.log 2>&1"
 
 # Import GPG public keys from /gpg folder
 for pubkey_file in /gpg/*.gpg; do
