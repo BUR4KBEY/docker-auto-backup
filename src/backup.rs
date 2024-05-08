471342f8822533115933bf3a8ae107e85d0daa67
BUR4KEY <rectfc@gmail.com>
2024-05-08 23:11:47 +0300

    feat: add direct container backups

--- src/backup.rs
+++ src/backup.rs
@@ -1,7 +1,13 @@
 use super::utils;
+use crate::{
+    config_parser::{BackupFile, Config},
+    spawn::spawn_child_process,
+};
+
 use std::process::Command;
 
 use chrono::{DateTime, Local};
+
 use tracing::{error, info};
 
 pub fn create_backup() -> String {
@@ -19,28 +25,12 @@ pub fn create_backup() -> String {
 
     let recipient = utils::get_env("GPG_RECIPIENT");
 
-    let mut child = {
-        match Command::new("bash")
-            .arg("generate_backup.sh")
-            .env("GPG_RECIPIENT", recipient)
-            .env("DATE", &date)
-            .spawn()
-        {
-            Ok(child) => child,
-            Err(e) => {
-                error!("failed to spawn child process\n\n{}", e);
-                std::process::exit(1);
-            }
-        }
-    };
-
-    match child.wait() {
-        Ok(_) => (),
-        Err(e) => {
-            error!("failed to wait for child process\n\n{}", e);
-            std::process::exit(1);
-        }
-    }
+    let mut command = Command::new("bash");
+    command
+        .arg("generate_backup.sh")
+        .env("GPG_RECIPIENT", recipient)
+        .env("DATE", &date);
+    spawn_child_process(&mut command);
 
     String::from(format!("{}.tar.zst.gpg", &date))
 }
@@ -63,6 +53,74 @@ pub fn cleanup(file_name: &str) {
         Err(e) => {
             error!("(cleanup) failed to delete file \"{}\"\n\n{}", file_name, e);
             std::process::exit(1);
+        }
+    }
+}
+
+pub async fn create_docker_containers_backup(config: &Config) {
+    for container in &config.containers {
+        if let Some(pre_build_script) = &container.pre_build_script {
+            for command_str in pre_build_script {
+                info!(
+                    "[{}] running pre-build script: \"{}\"",
+                    &container.name, command_str
+                );
+
+                let mut command = Command::new("docker");
+                command
+                    .arg("exec")
+                    .arg("-it")
+                    .arg(&container.name)
+                    .arg("sh")
+                    .arg("-c")
+                    .arg(&command_str);
+                spawn_child_process(&mut command);
+
+                info!("[{}] done", &container.name);
+            }
+        }
+
+        for BackupFile(target_path, backup_path) in &container.files {
+            info!(
+                "[{}] creating the local path \"{}\"",
+                &container.name, backup_path
+            );
+
+            let mut mkdir_command = Command::new("mkdir");
+            mkdir_command.arg("-p").arg(backup_path);
+            spawn_child_process(&mut mkdir_command);
+
+            info!("[{}] copying files to the local path", &container.name);
+
+            let mut cp_command = Command::new("docker");
+            cp_command
+                .arg("cp")
+                .arg(format!("{}:{}", &container.name, target_path))
+                .arg(backup_path);
+            spawn_child_process(&mut cp_command);
+
+            info!("[{}] done", &container.name);
+        }
+
+        if let Some(post_build_script) = &container.post_build_script {
+            for command_str in post_build_script {
+                info!(
+                    "[{}] running post-build script: \"{}\"",
+                    &container.name, command_str
+                );
+
+                let mut command = Command::new("docker");
+                command
+                    .arg("exec")
+                    .arg("-it")
+                    .arg(&container.name)
+                    .arg("sh")
+                    .arg("-c")
+                    .arg(&command_str);
+                spawn_child_process(&mut command);
+
+                info!("[{}] done", &container.name);
+            }
         }
     }
 }
